f1ae566a805d1c6555f588736e474b33a4945f25
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2024-11-24 19:46:39 -0300

    Resolvendo TODO's

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia(numero: "1234", modelo: "Tijolão", imei: "11111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Signal");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone");
+        Smartphone iphone = new Iphone(numero: "5678", modelo: "MAX", imei: "22222", memoria: 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Senhas");
+    }
+}
